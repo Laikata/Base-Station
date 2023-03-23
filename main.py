032e534c47125d6d4674507d2369521d52c8419b
Pedro-Signes <pedrossignes2005@gmail.com>
2023-03-23 20:17:24 +0100

    changed to sockets instead of post

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import random
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import threading
 import time
+import websockets
+import asyncio
+
+activeSockets = []
 
 
 data = {"lat":-1,
@@ -12,13 +16,33 @@ data = {"lat":-1,
         "hum":-1,
         "heading":-1}
 
+async def sendWebsockets():
+    print("sending to", len(activeSockets), "sockets")
+    for sock in activeSockets:
+        
+        await sock.send(str(data))
+
 def serialListener():
     while True:
         time.sleep(0.5)
         data["long"] = random.random()
         data["lat"] = random.random()
+        asyncio.run(sendWebsockets())
 
+async def handle_connection(websocket, path):
+    activeSockets.append(websocket)
+    print("WebSocket connection established and added to the list")
+    while websocket.open:
+        pass
+    print("websocket was closed")
+    activeSockets.remove(websocket)
 
+async def websockets_server():
+    print("starting websocket listener")
+    
+    async with websockets.serve(handle_connection, "localhost", 8765, ping_interval=2, ping_timeout=2):
+        await asyncio.Future()
+    
 
 class Server(SimpleHTTPRequestHandler):
 
@@ -31,12 +55,15 @@ class Server(SimpleHTTPRequestHandler):
         else:
             SimpleHTTPRequestHandler.do_GET(self)
 
-if __name__ == '__main__':
-
-    serialThread = threading.Thread(target=serialListener)
-    serialThread.start()
-
+def http_server():
+    print("stargin server")
     server_address = ('0.0.0.0', 80)
     httpd = HTTPServer(server_address, Server)
     httpd.serve_forever()
+
+if __name__ == '__main__':
+    threading.Thread(target=http_server).start()
+    threading.Thread(target=serialListener).start()
+    asyncio.run(websockets_server())
+
 
--- updateData.js
+++ updateData.js
@@ -27,6 +27,37 @@ $( document ).ready(function() {
     L.marker([51.5, -0.09]).addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
+
+    const socket = new WebSocket("ws://localhost:8765/");
+
+    // add an event listener for the "open" event, which is fired when the connection is established
+    socket.addEventListener('open', () => {
+        console.log('WebSocket connection established');
+    });
     
+    // add an event listener for the "message" event, which is fired when a message is received
+    socket.addEventListener('message', (event) => {
+        console.log('Received message:', event.data);
+        data = JSON.parse(event.data);
+        var dataHtml = ""
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                dataHtml += "<p>" + key + " : " + data[key] + "</p>"
+                
+               console.log(data[key]);
+            }
+         }
+         $(".result").html(dataHtml);
+    });
+    
+    // add an event listener for the "close" event, which is fired when the connection is closed
+    socket.addEventListener('close', () => {
+        console.log('WebSocket connection closed');
+    });
+    
+    // send a message over the WebSocket connection
+    socket.send('Hello, server!');
+        
+
 })
 
